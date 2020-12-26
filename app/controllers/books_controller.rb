7907831b48372d237e1b4c600d92f95b36f9a1f6
zuotengyui1@gmail.com <ec2-user@ip-172-31-47-237.ec2.internal>
2020-12-26 07:31:27 +0000

    rails_課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
     flash[:notice] = 'Book was successfully created.'
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
     else
-  		@books = Book.all
-  		render action: :index
+    @books = Book.all
+    render 'index'
     end
   end
 
@@ -28,15 +28,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = 'Book was successfully created.'
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:destroy] = 'Book was successfully destroyed.'
     redirect_to books_path
   end
 
